6f8ff6b272ac95596e1ff4b5194c2b2bce5ec326
Saman Hazemi <samanhazemi1383@gmail.com>
2020-06-05 20:16:15 +0430

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long int n=2,z=1,k;
+    for(int i =1;100>=i;i++)
+    {
+        k=n;
+        n += z;
+            cout << n << endl;
+            z = k;
+
+    }
+    
+}
